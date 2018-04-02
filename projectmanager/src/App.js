40c29ab2f93810acff25158805256c7f8b5cda91
Bob Brown <robertbernardbrown@gmail.com>
2018-04-02 14:28:34 -0400

    small changes

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 import Projects from "./Components/projects"
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: "Business Website",
+          category: "web design"
+        },
+        {
+          title: "Social App",
+          category: "mobile dev"
+        },
+        {
+          title: "ECommerce Shopping Cart",
+          category: "web development"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         My App
-        <Projects test="Hello World!"/>
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
---
+++ projectmanager/src/Components/projectItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class projectItem extends Component {    
+  render() {
+      console.log(this.props);
+    return (
+      <li className="project">
+        
+      </li>
+    );
+  }
+}
+
+export default projectItem;
