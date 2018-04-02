84ed3eee4e61a677a688445c7e4701b4b9a7f3d7
Bob Brown <robertbernardbrown@gmail.com>
2018-04-02 14:09:05 -0400

    makes initial project files

---
+++ projectmanager/src/Components/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class projects extends Component {
+  render() {
+    return (
+      <div className="projects">
+        My projects
+      </div>
+    );
+  }
+}
+
+export default projects;
